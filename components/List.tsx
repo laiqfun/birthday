04bcbf7138a48c2d514e5bd5dc571b68dec8424d
laizq <123632704@qq.com>
2023-06-02 23:08:07 +0800

    key

--- components/List.tsx
+++ components/List.tsx
@@ -1,24 +1,25 @@
 export default function List({
   content,
-  onClick=()=>{},
+  onClick = () => {},
 }: {
-  onClick(k:number): unknown;
+  onClick(k: number): unknown;
   content: {
     text: string;
   }[];
 }) {
   return (
     <div className={"flex flex-col p-2"}>
-      {content.map((c,k) => {
+      {content.map((c, k) => {
         return (
-          <div
-            className={
-              "border my-1 px-1 py-2 cursor-pointer text-sm text-gray-600"
-            }
-            key={k}
-            onClick={()=>onClick(k)}
-          >
-            {c.text}
+          <div key={k}>
+            <div
+              className={
+                "border my-1 px-1 py-2 cursor-pointer text-sm text-gray-600"
+              }
+              onClick={() => onClick(k)}
+            >
+              {c.text}
+            </div>
           </div>
         );
       })}
